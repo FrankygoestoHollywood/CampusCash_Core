6a6c321b72c32b07d31d73005d6b93cb14949529
SaltineChips <rubixblockchain@gmail.com>
2021-08-07 12:16:42 -0700

    gitignore update

--- src/main.h
+++ src/main.h
@@ -84,7 +84,7 @@ static const int64_t nDrift = 5 * 60;
 /** Future drift params */
 inline int64_t FutureDrift(int64_t nTime) { return nTime + nDrift; }
 /** Velocity Factor handling toggle */
-inline bool FACTOR_TOGGLE(int nHeight) { return TestNet() || nHeight > 445000; } // Checks initiate after block 445000
+inline bool FACTOR_TOGGLE(int nHeight) { return TestNet() || nHeight > 442835; } // One block past issue block: 442619
 /** "reject" message codes **/
 static const unsigned char REJECT_INVALID = 0x10;
 
