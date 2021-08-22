1dd3e217f4ea4ae9f53129901e2f0c46e89ea5b4
SaltineChips <rubixblockchain@gmail.com>
2021-08-21 18:11:18 -0700

    Commit 1.1.0.12

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -554,7 +554,7 @@ void CampusCashGUI::createToolBars()
 
 void CampusCashGUI::setClientModel(ClientModel *clientModel)
 {
-    netLabel->setText("v1.1.0.11");// Version in GUI
+    netLabel->setText("v1.1.0.12");// Version in GUI
 
     this->clientModel = clientModel;
     if(clientModel)
